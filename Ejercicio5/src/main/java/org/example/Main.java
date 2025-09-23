d89a174eb20033ad7c721d665ce5703035801089
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-09-23 21:29:38 +0200

    Ejercicio 5 terminado

--- Ejercicio5/src/main/java/org/example/Main.java
+++ Ejercicio5/src/main/java/org/example/Main.java
@@ -3,24 +3,28 @@ package org.example;
 import java.io.IOException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Scanner tec = new Scanner(System.in);
         int exitCode;
-        System.out.println("Introduce un comando para ejecutar, y sus parametros si los tiene(separa cada uno con un espacio):");
-        String comando = tec.nextLine();
-        String[] partes = comando.split(" ");
-        ProcessBuilder pb = new ProcessBuilder(partes);
-        try {
-            Process p = pb.start();
-            exitCode = p.waitFor();
-            System.out.println(exitCode);
-        } catch (IOException | InterruptedException e) {
-            System.out.println("Error al ejecutar el comando: " + e.getMessage() );
-        }
-
+        String comando;
+        do {
+            System.out.println("Introduce un comando para ejecutar, y sus parametros si los tiene(separa cada uno con un espacio):");
+            System.out.println("si quieres salir escriba la palabra 'salir'");
+            comando = tec.nextLine();
+            if(comando.isEmpty()) // Si el String está vacio  volver saltarse, volver a iterar
+                continue;
+            var partes = comando.split(" "); //Convierte a un array de Strings la palabra leida
+            ProcessBuilder pb = new ProcessBuilder(partes);
+            try {
+                Process p = pb.start();
+                exitCode = p.waitFor();
+                System.out.println("El codigo de finalizacion es "+exitCode);
+            } catch (IOException | InterruptedException e) {
+                System.out.println("Error al ejecutar el comando: " + e.getMessage());
+            }
+        } while (!comando.equals("salir"));
 
         tec.close();
     }
