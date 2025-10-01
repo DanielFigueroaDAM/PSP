568613a9b18357cc3d0754da149ef0f7f5ef0c13
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-10-01 18:01:20 +0200

    refactorizando

--- Ejercicio11/src/main/java/org/example/Main.java
+++ Ejercicio11/src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static Thread[] hilos = new Thread[5];
     public static void main(String[] args) {
@@ -14,32 +13,25 @@ public class Main {
 
     public static void crearHilos() {
         for (int i = 0; i < hilos.length; i++) {
-            hilos[i] = new Thread(() -> lazadorHilos());
+            hilos[i] = new Thread(Main::lazadorHilos); // Se le pasa la funcion anonima(Method Reference)
             hilos[i].setName("Hilo " + (i + 1));
         }
     }
     static int hilo = 0;
     public static void lazadorHilos(){
-        for (int i = 0; i < 5; i++) {
-            System.out.println("["+Thread.currentThread().getName() +"]" + "Iteracion " + (i+1));
-            try {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("["+Thread.currentThread().getName() +"]" + "Iteracion " + (i+1));
                 Thread.sleep(500);
-            } catch (InterruptedException e) {
-                System.out.println("["+Thread.currentThread().getName() +"]" + "Interrumpido");
-                return;
-            }
-            if (i == 4) {
-                hilo++;
-                if (hilo < hilos.length) {
-                    hilos[hilo].start();
-                    try {
-                        hilos[hilo].join();
-                    } catch (InterruptedException e) {
-                        System.out.println("[" + Thread.currentThread().getName() + "]" + "Interrumpido");
-                        return;
-                    }
+                if (i == 4 && ++hilo < hilos.length) { // Hacemos el preincremento de la variable hilo y se comprueba para no pasarse del array
+                    hilos[hilo].start();// llama al siguiente hilo que ejecuta este metodo(parece recursivo pero no lo es)
+                    hilos[hilo].join();
                 }
             }
+        }
+        catch (InterruptedException er) {
+            System.out.println("[" + Thread.currentThread().getName() + "]" + "Interrumpido");
+            return;
         }
         System.out.println("["+Thread.currentThread().getName() +"]" + "Termina");
 
