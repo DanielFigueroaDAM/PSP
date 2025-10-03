cb032b141a2326f1016fe718d1f688b306f3ebf6
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-10-03 14:09:24 +0200

    producto creado

---
+++ Practica1-3IOSerializacioneXML/src/main/java/org/example/Producto.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class Producto implements Serializable {
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String nombre, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                ", precio=" + precio +
+                '}';
+    }
+}
