af46c7411dd390fdfd10a388bc6247d26656f9a2
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-10-08 09:24:17 +0200

    tarea 13 primer apartado terminado

--- Ejercicio13/src/main/java/org/example/HiloT13.java
+++ Ejercicio13/src/main/java/org/example/HiloT13.java
@@ -1,4 +1,25 @@
 package org.example;
 
-public class HiloT13 {
+public class HiloT13 extends Thread {
+
+    public HiloT13(String name){
+        setName(name);
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i<10;i++){
+            System.out.println("["+getName()+"]"+"Estamos en la iteracion "+i);
+            try {
+                sleep(tiempoAleatorio());
+            } catch (InterruptedException e) {
+                System.out.println("Fallo al dormir el hilo: "+e);
+            }
+
+        }
+    }
+    public static int tiempoAleatorio(){
+        return (int) (Math.random()*9001)+1000;
+    }
 }
+
+
