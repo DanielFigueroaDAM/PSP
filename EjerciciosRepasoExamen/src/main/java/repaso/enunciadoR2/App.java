e6618a8efb358f81616d7414ab4ba3af459c888a
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-10-25 18:55:11 +0200

    todos los ejercicios del boletin terminados

--- EjerciciosRepasoExamen/src/main/java/repaso/enunciadoR2/App.java
+++ EjerciciosRepasoExamen/src/main/java/repaso/enunciadoR2/App.java
@@ -1,13 +1,21 @@
 package repaso.enunciadoR2;
 
 public class App {
-    private static int fase = 0;
 
     public static void main(String[] args) {
         Object l1 = new Object();
         Operario o1 = new Operario(l1,1);
         Operario o2 = new Operario(l1,2);
         Operario o3 = new Operario(l1,3);
+
+        o1.start(); o2.start(); o3.start();
+
+        try{
+            o1.join(); o2.join(); o3.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
--- EjerciciosRepasoExamen/src/main/java/repaso/enunciadoR2/Operario.java
+++ EjerciciosRepasoExamen/src/main/java/repaso/enunciadoR2/Operario.java
@@ -10,54 +10,58 @@ public class Operario extends Thread{
 
     @Override
     public void run() {
-        synchronized (lock){
-
             switch (fase) {
-                case 1:
-                    cogerPieza();
-                    break;
-                case 2:
-                    pintarPieza();
-                    break;
-                case 3:
-                    embalandoPieza();
-                    break;
+                case 1 -> cogerPieza();
+                case 2 -> pintarPieza();
+                case 3 -> embalandoPieza();
             }
-
-        }
     }
 
     public void cogerPieza(){
-        synchronized (lock) {
-            try {
+        try {
+
+            synchronized (lock) {
+                while (EstadosProduccion.getFaseProducto() != 0) {
+                    lock.wait();
+                }
                 System.out.println("Cogiendo pieza");
-                sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                EstadosProduccion.avanzarProduccion();
+                lock.notifyAll();
             }
-            fase++;
+            sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
     }
     public void pintarPieza(){
-        synchronized (lock) {
-            try {
+        try {
+            synchronized (lock) {
+                while (EstadosProduccion.getFaseProducto() != 1) {
+                    lock.wait();
+                }
                 System.out.println("Pintando la pieza");
                 sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                EstadosProduccion.avanzarProduccion();
+                lock.notifyAll();
             }
-            fase++;
+            sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
     public void embalandoPieza(){
-        synchronized (lock) {
-            try {
+
+        try {
+            synchronized (lock) {
+                while (EstadosProduccion.getFaseProducto() != 2) {
+                    lock.wait();
+                }
                 System.out.println("Embalando la pieza");
-                sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
             }
+            sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        fase++;
     }
 }
