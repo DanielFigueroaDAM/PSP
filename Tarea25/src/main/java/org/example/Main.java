2560cc4481a77d282290fa997617252a4980c937
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-11-04 12:29:56 +0100

    empezando ejercicio 25 de psp

---
+++ Tarea25/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Dame la ip");
+        String ip = tec.next();
+
+
+    }
+}
